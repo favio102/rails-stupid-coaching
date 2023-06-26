7432f583e8aa04e8fbdd8bdfe3a6968c2028ae24
Favio Valero <favio102@protonmail.com>
2023-06-25 23:14:11 -0700

    new commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @ask = params[:answer]
+    @answer = if @ask && @ask.downcase == 'i am going to work'
+                'Great!'
+              elsif @ask && @ask.ends_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and got to work!"
+              end
+  end
+end
